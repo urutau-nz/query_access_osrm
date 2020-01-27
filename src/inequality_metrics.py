f6f051a7500e91c1ea46231dba818f29e07cb85c
MitchellAnderson112 <man112@uclive.ac.nz>
2020-01-28 10:55:33 +1300

    New file

---
+++ src/inequality_metrics.py
@@ -0,0 +1,65 @@
+'''
+The goal of this script is to do any of the following:
+For supermarkets in a given city:
+    1. Calculate the EDEs and Indices
+    2. Plot a population weight Histogram with summary statistics
+    3. Plot a gini curve
+    4. Plot a CDF
+    5. Return a CSV file with all EDEs, Indices, and Summary statistics
+For a list of races/ethnicities within a given city:
+    1. Calculate the EDEs and Indices for each race
+    2. Plot a population weight Histogram with summary statistics for each race
+    3. Plot a gini curve for each race
+    4. Plot a CDF for each race
+    5. Return a CSV file with all EDEs, Indices, and Summary statistics
+For a list of cities:
+    1. Calculate the EDEs and Indices
+    2. Plot a population weight Histogram with summary statistics
+    3. Plot a gini curve
+    4. Plot a CDF
+    5. Return a CSV file with all EDEs, Indices, and Summary statistics
+'''
+
+# User defined variables
+# Will compare the
+beta = -0.5
+compare_city = False
+states = ['fl', 'co', 'mi', 'la', 'ga', 'or', 'tx', 'il', 'wa', 'md']
+compare_race = False
+races = ['all', 'white', 'non_white', 'black', 'american_indian', 'asian', 'hispanic'] #Black and african american, indiand and native alaskan, hispanic and latino
+file_name = ''
+
+import utils
+from config import *
+
+
+def main():
+    kapa = calc_kapa()
+
+
+def calc_kapa():
+    dfs = []
+    kapa_data = []
+    for state in states:
+        db, context = cfg_init(state)
+        cursor = db['con'].cursor()
+        sql = 'SELECT * FROM nearest_dist'
+        exec(f'{state} = pd.read_sql(sql, df["con"])')
+        sql = 'SELECT * FROM demograph'
+        exec(f'{state}_demo = pd.read_sql(sql, db["con"])')
+        db['con'].close()
+        dfs.append({state})
+    print(dfs)
+        #exec(f'{state} = {state}.sort_values(by="geoid10")')
+        #exec(f'{state}_demo = {state}.sort_values(by="geoid10")')
+        #exec(f'{state}["pop_all"] = {state}_demo["H7X001"]')
+        #exec(f'{state}["pop_white"] = {state}_demo["H7X002"]')
+        #exec(f'{state}["pop_non_white"] = {state}_demo["H7X001"] - {state}_demo["H7X002"]')
+        #exec(f'{state}["pop_black"] = {state}_demo["H7X003"]')
+        #exec(f'{state}["pop_american_indian"] = {state}_demo["H7X004"]')
+        #exec(f'{state}["pop_asian"] = {state}_demo["H7X005"]')
+        #exec(f'{state}["pop_hispanic"] = {state}_demo["H7Y003"]')
+        #for distance in {state}
+    print(dfs)
+
+calc_kapa()
