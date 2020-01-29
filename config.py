21f74e912d3dde28dab557fcf72498dc071024df
MitchellAnderson112 <man112@uclive.ac.nz>
2020-01-29 13:59:50 +1300

    test

---
+++ config.py
@@ -0,0 +1,112 @@
+'''
+Imports functions and variables that are common throughout the project
+'''
+
+# functions - data analysis
+import numpy as np
+import pandas as pd
+import itertools
+# functions - geospatial
+import geopandas as gpd
+# functions - data management
+import pickle as pk
+import psycopg2
+from sqlalchemy.engine import create_engine
+# functions - coding
+import code
+import os
+from datetime import datetime, timedelta
+import time
+from tqdm import tqdm
+#plotting
+from scipy.integrate import simps
+# logging
+import logging
+logging.basicConfig(
+    format='%(asctime)s %(levelname)-8s %(message)s',
+    level=logging.INFO,
+    datefmt='%Y-%m-%d %H:%M:%S')
+
+def cfg_init(state):
+    # SQL connection
+    db = dict()
+    db['passw'] = open('pass.txt', 'r').read().strip('\n')
+    db['host'] = '132.181.102.2'
+    db['port'] = '5001'
+    # city information
+    context = dict()
+    if state == 'md':
+        db['name'] = 'access_md'
+        context['city_code'] = 'bal'
+        context['city'] = 'baltimore'
+        # url to the osrm routing machine
+        context['osrm_url'] = 'http://localhost:6003'
+        context['services'] = ['supermarket', 'school', 'hospital', 'library']
+    elif state == 'wa':
+        db['name'] = 'access_wa'
+        context['city_code'] = 'sea'
+        context['city'] = 'seattle'
+        # url to the osrm routing machine
+        context['osrm_url'] = 'http://localhost:6004'
+        context['services'] = ['supermarket', 'school', 'hospital', 'library']
+    elif state == 'nc':
+        db['name'] = 'access_nc'
+        context['city_code'] = 'wil'
+        context['city'] = 'wilmington'
+        # url to the osrm routing machine
+        context['osrm_url'] = 'http://localhost:6002'
+        context['services'] = ['super_market_operating', 'gas_station']
+    elif state == 'il':
+        db['name'] = 'access_il'
+        context['city_code'] = 'chi'
+        context['city'] = 'Chicago'
+        context['osrm_url'] = 'http://localhost:6005'
+        context['services'] = ['supermarket']
+    elif state == 'tx':
+        db['name'] = 'access_tx'
+        context['city_code'] = 'hou'
+        context['city'] = 'Houston'
+        context['osrm_url'] = 'http://localhost:6006'
+        context['services'] = ['supermarket']
+        context['services'] = ['supermarket']
+    elif state == 'or':
+        db['name'] = 'access_or'
+        context['city_code'] = 'por'
+        context['city'] = 'Portland'
+        context['osrm_url'] = 'http://localhost:6007'
+        context['services'] = ['supermarket']
+    elif state == 'ga':
+        db['name'] = 'access_ga'
+        context['city_code'] = 'atl'
+        context['city'] = 'Atlanta'
+        context['osrm_url'] = 'http://localhost:6008'
+        context['services'] = ['supermarket']
+    elif state == 'la':
+        db['name'] = 'access_la'
+        context['city_code'] = 'new'
+        context['city'] = 'New_Orleans'
+        context['osrm_url'] = 'http://localhost:6009'
+        context['services'] = ['supermarket']
+    elif state == 'mi':
+        db['name'] = 'access_mi'
+        context['city_code'] = 'det'
+        context['city'] = 'Detroit'
+        context['osrm_url'] = 'http://localhost:6010'
+        context['services'] = ['supermarket']
+    elif state == 'co':
+        db['name'] = 'access_co'
+        context['city_code'] = 'den'
+        context['city'] = 'Denver'
+        context['osrm_url'] = 'http://localhost:6011'
+        context['services'] = ['supermarket']
+    elif state == 'fl':
+        db['name'] = 'access_fl'
+        context['city_code'] = 'mia'
+        context['city'] = 'Miami'
+        context['osrm_url'] = 'http://localhost:6012'
+        context['services'] = ['supermarket']
+    # connect to database
+    db['engine'] = create_engine('postgresql+psycopg2://postgres:' + db['passw'] + '@' + db['host'] + '/' + db['name'] + '?port=' + db['port'])
+    db['address'] = "host=" + db['host'] + " dbname=" + db['name'] + " user=postgres password='"+ db['passw'] + "' port=" + db['port']
+    db['con'] = psycopg2.connect(db['address'])
+    return(db, context)
---
+++ utils.py
@@ -0,0 +1,93 @@
+'''
+General functions that support the project
+- import_csv for csv -> postgres db
+-
+'''
+# user defined variables
+state = input('State: ')
+from config import *
+db, context = cfg_init(state)
+
+import yagmail
+
+
+def send_email(body):
+    # send an email
+
+    receiver = "man112@uclive.ac.nz"
+    # filename = "document.pdf"
+
+    yag = yagmail.SMTP('toms.scrapers',open('pass_email.txt', 'r').read().strip('\n'))
+    yag.send(
+        to=receiver,
+        subject="Your code notification",
+        contents=body,
+        # attachments=filename,
+    )
+
+
+def import_csv(db):
+    '''
+    import a csv into the postgres db
+    '''
+    if state=='md':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '510'
+    elif state == 'wa':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '033'
+    elif state == 'nc':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '129'
+    elif state == 'il':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '031'
+    elif state == 'tx':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '201'
+    elif state == 'or':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '051'
+    elif state == 'ga':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '121'
+    elif state == 'la':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '071'
+    elif state == 'mi':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '163'
+    elif state == 'co':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '031'
+    elif state == 'fl':
+        file_name = '/homedirs/man112/access_inequality_index/data/usa/{}/{}/demo/demo.csv'.format(state, context['city_code'])
+        county = '086'
+    #
+    table_name = 'demograph'
+    # add csv to nc.demograph
+    df = pd.read_csv(file_name, dtype = {'STATEA':str, 'COUNTYA':str,'TRACTA':str,'BLOCKA':str, 'H7X001':int, 'H7X002':int, 'H7X003':int, 'H7X004':int, 'H7X005':int, 'H7Y003':int})
+    df = df[df.COUNTYA==county]
+    df['geoid10'] = df['STATEA'] + df['COUNTYA'] + df['TRACTA'] + df['BLOCKA']
+
+    df_filt = pd.DataFrame()
+    df_filt['geoid10'] = df['geoid10']
+    df_filt['H7X001'] = df['H7X001']
+    df_filt['H7X002'] = df['H7X002']
+    df_filt['H7X003'] = df['H7X003']
+    df_filt['H7X004'] = df['H7X004']
+    df_filt['H7X005'] = df['H7X005']
+    df_filt['H7Y003'] = df['H7Y003']
+    df_filt.to_sql(table_name, db['engine'])
+    # add the table indices
+
+    cursor = db['con'].cursor()
+    queries = ['CREATE INDEX "geoid10" ON demograph ("geoid10");']
+    for q in queries:
+        cursor.execute(q)
+    # commit
+    db['con'].commit()
+
+
+if __name__ == '__main__':
+    import_csv(db)
