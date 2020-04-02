66fba5c4d590974fcab3fd337a6fa30ccbc11bd0
Michael Freeman <michaelfreeman100@gmail.com>
2020-04-02 14:28:12 +1300

    Need to get the coordiante lists 2

--- src/query.py
+++ src/query.py
@@ -203,7 +203,8 @@ def execute_route_query(origxdest, orig_df, dest_df):
 def execute_table_query(origxdest):
     #here we want a for loop of string comp to build the query with the table instead of creating shitloads of induvidual queries
     id_orig = origxdest['id_orig']
-    print(id_orig)
+    print(id_orig[100])
+    print(id_orig['x'])
     iterator = 0
     destination_string = "&destinations="
     source_string = "?sources="
