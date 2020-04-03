1741c367c2e6e3d05875f1c0b1e7cbb3b4e78210
Michael Freeman <michaelfreeman100@gmail.com>
2020-04-03 21:56:55 +1300

    More git spam so i can test on the server

--- src/query.py
+++ src/query.py
@@ -201,9 +201,9 @@ def execute_route_query(origxdest, orig_df, dest_df):
     return origxdest
 
 
-def execute_table_query(origxdest):
+def execute_table_query(origxdest, orig_df, dest_df):
     #here we want a for loop of string comp to build the query with the table instead of creating shitloads of induvidual queries
-    id_orig = origxdest['id_orig'].values
+    print(orig_df[1])
     iterator = 0
     destination_string = "&destinations="
     source_string = "?sources="
