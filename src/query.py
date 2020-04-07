fecba4addb05c1a0656fb42068dace0bcd930835
Michael Freeman <michaelfreeman100@gmail.com>
2020-04-07 17:16:57 +1200

    Even more bugfix and analysis

--- src/query.py
+++ src/query.py
@@ -237,6 +237,7 @@ def execute_table_query(origxdest, orig_df, dest_df):
         response = requests.get(table_query_string)
         #now to proccess the response
         print(response.json())
+        print(table_query_string[3836:3840])
     #need to process r here to get the required info
 
     
