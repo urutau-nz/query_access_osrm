1f494caf1f7ef203bac20bd81289bc1466dfbc04
Michael Freeman <michaelfreeman100@gmail.com>
2020-04-14 15:19:40 +1200

    Test query

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -1,6 +1,8 @@
 # the query looks like this: '{}/route/v1/driving/{},{};{},{}?overview=false'.format(osrm_url, lon_o, lat_o, lon_d, lat_d)
 # http://localhost:6014/route/v1/driving/{},{};{},{}
 
+
+#Loc X: -90.071392618565, Loc Y: 30.01270094914956, dest X: -90.116577, dest Y-90.116577
 import requests
 
-response = requests.get("http://localhost:6014/route/v1/driving/{},{};{},{}".format())
+response = requests.get("http://localhost:6014/route/v1/driving/{},{};{},{}".format(-90.071392618565, 30.01270094914956, -90.116577, -90.116577))
