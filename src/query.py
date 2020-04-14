3fcf612c7bdf85caf8e2748524d29e14ec25ea8b
Michael Freeman <michaelfreeman100@gmail.com>
2020-04-14 15:24:33 +1200

    Fixed some errors

--- src/query.py
+++ src/query.py
@@ -254,8 +254,8 @@ def execute_table_query(origxdest, orig_df, dest_df):
         #now to proccess the response
         for dest_string in response.json()['destinations'] :
             #this is temp
-            temp_origxdest.append([query_wrapper.orig_loc_x, query_wrapper.orig_loc_y, dest_string['location'][0], dest_string['location'][0], dest_string['distance']])
-            print("Loc X: {}, Loc Y: {}, dest X: {}, dest Y{}".format(query_wrapper.orig_loc_x, query_wrapper.orig_loc_y, dest_string['location'][0], dest_string['location'][0]))
+            temp_origxdest.append([query_wrapper.orig_loc_x, query_wrapper.orig_loc_y, dest_string['location'][0], dest_string['location'][1], dest_string['distance']])
+            print("Loc X: {}, Loc Y: {}, dest X: {}, dest Y{}".format(query_wrapper.orig_loc_x, query_wrapper.orig_loc_y, dest_string['location'][0], dest_string['location'][1]))
 
             #locate the pair
             #print(dest_string['distance'])
