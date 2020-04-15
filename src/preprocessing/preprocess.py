ab1d0130cca7bf0d6de5133c4ab7c41619839595
Michael Freeman <michaelfreeman100@gmail.com>
2020-04-15 16:00:33 +1200

    Added options to query

--- src/preprocessing/preprocess.py
+++ src/preprocessing/preprocess.py
@@ -5,6 +5,52 @@
 #Loc X: -90.071392618565, Loc Y: 30.01270094914956, dest X: -90.385078, dest Y29.901669
 import requests
 
-response = requests.get("http://localhost:6014/route/v1/driving/{},{};{},{}".format(-90.071392618565, 30.01270094914956, -90.385078, 29.901669))
+response = requests.get("http://localhost:6014/route/v1/driving/{},{};{},{}&annotations=true&steps=true".format(-90.071392618565, 30.01270094914956, -90.385078, 29.901669))
+
+#http://project-osrm.org/docs/v5.15.2/api/?language=Python#waypoint-object
 
 print(response.json())
+
+'''
+{
+   "code":"Ok",
+   "routes":[
+      {
+         "geometry":"ezdvD~cwdPfk@gHhTzT~jBeGhmB__A`\\u@rF}IlRnPfKiEng@jFySqp@pYcRvyC}GrvAxpCnj@hfDeNf@xH`[lCrjA{i@|uByN~RchAf^ob@dgB^vuApi@dRdTO`HllBeKfZfB`\\}E`i@oVxd@cj@hb@gfCd|@yRbOgm@~gAuKjz@pKd_Bt]`zApe@~z@f_C`kCvJhw@oA|vBaVzjB~iEhuCzHcDyGcO",
+         "legs":[
+            {
+               "steps":[],
+               "distance":54765.5,
+               "duration":39434.4,
+               "summary":"",
+               "weight":39434.4
+            }
+         ],
+         "distance":54765.5,
+         "duration":39434.4,
+         "weight_name":"duration",
+         "weight":39434.4
+      }
+   ],
+   "waypoints":[
+      {
+         "hint":"SvoHgJX6B4CLAgAAlAIAAAAAAAAAAAAA_km0QqODtkIAAAAAAAAAAIsCAACUAgAAAAAAAAAAAAABAAAA5Zyh-gH1yQGfnqH6HfXJAQAA_wrbHa5x",
+         "distance":42.749924,
+         "name":"Charlotte Drive",
+         "location":[
+            -90.071835,
+            30.012673
+         ]
+      },
+      {
+         "hint":"O1cKgD5XCoC5AAAAEAEAAAAAAAAAAAAAyTHNQTxlFkIAAAAAAAAAALkAAAAQAQAAAAAAAAAAAAABAAAAStWc-mVDyAFK1Zz6ZUPIAQAAnwHbHa5x",
+         "distance":0,
+         "name":"Magnolia Avenue",
+         "location":[
+            -90.385078,
+            29.901669
+         ]
+      }
+   ]
+}
+'''
