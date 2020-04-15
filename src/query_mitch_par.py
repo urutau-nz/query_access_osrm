f796a3928591b4020dd4f7740fd4f826de35216e
MitchellAnderson112 <man112@uclive.ac.nz>
2020-04-15 23:23:20 +1200

    test retries

--- src/query_mitch_par.py
+++ src/query_mitch_par.py
@@ -235,6 +235,7 @@ def execute_table_query(origxdest, orig_df, dest_df):
 
 def req(query_wrapper):
     #for query_wrapper in tqdm(query_list):
+    requests_retry_session(retries=100, backoff_factor=0.01, status_forcelist=(500, 502, 504), session=None).get(query_wrapper.query_string)
     response = requests.get(query_wrapper.query_string)
     temp_dist = response.json()['distances']#[0][1:]
     #temp_origxdest = []
