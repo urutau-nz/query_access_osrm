2b40eeeb98a73cbce86e5c44728aabb99b5602b0
Tom Logan <tom.logan@canterbury.ac.nz>
2020-04-17 23:25:32 +1200

    tidying

--- src/init_osrm.py
+++ src/init_osrm.py
@@ -10,8 +10,4 @@ transport_mode = 'car'
 directory = '/homedirs/man112/osm_data'
 
 
-import subprocess
-print "start"
-# subprocess.call("sleep.sh")
-subprocess.check_call("./init_osrm.sh %s %s %s %s %s" % (state_name, port, transport_mode, directory, state))
-print "end"
+subprocess.check_call(['./init_osrm.sh', state_name, port, transport_mode, directory, state])
