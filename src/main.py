31f2bd84e5a66472c102624b8c77c5f1908a47f4
Tom Logan <tom.logan@canterbury.ac.nz>
2020-12-21 18:52:03 +1300

    fixed the polygon export tool

--- src/main.py
+++ src/main.py
@@ -7,6 +7,13 @@ import psycopg2
 from sqlalchemy.types import Float, Integer
 from sqlalchemy import create_engine
 from sqlalchemy_utils import database_exists, create_database
+# functions - geospatial
+import osgeo.ogr
+import geopandas as gpd
+import shapely
+from shapely.geometry.polygon import Polygon
+from shapely.geometry.multipolygon import MultiPolygon
+from geoalchemy2 import Geometry, WKTElement
 # functions - logging
 import logging
 logging.basicConfig(
@@ -34,6 +41,12 @@ def main(config_filename=None):
 
     # initialize and connect to the server
     db = init_db(config)
+
+    # add origins and destinations
+    # init_destinations(db, config)
+    init_origins(db, config)
+    db['con'].close()
+
     # query.main(config)
 
     # shutdown the OSRM server
@@ -53,49 +66,70 @@ def init_db(config):
     # SQL connection
     db = config['SQL'].copy()
     db['passw'] = open('pass.txt', 'r').read().strip('\n')
+    db['engine'] = create_engine('postgresql+psycopg2://postgres:' + db['passw'] + '@' + db['host'] + '/' + db['database_name'] + '?port=' + db['port'])
+    db['address'] = "host=" + db['host'] + " dbname=" + db['database_name'] + " user=postgres password='"+ db['passw'] + "' port=" + db['port']
 
     # Create the database
-    db['engine'] = create_engine('postgresql+psycopg2://postgres:' + db['passw'] + '@' + db['host'] + '/' + db['database_name'] + '?port=' + db['port'])
-    if not database_exists(db['engine'].url):
+    exists = database_exists(db['engine'].url)
+    if not exists:
         create_database(db['engine'].url)
 
     # connect to database
-    db['address'] = "host=" + db['host'] + " dbname=" + db['database_name'] + " user=postgres password='"+ db['passw'] + "' port=" + db['port']
     db['con'] = psycopg2.connect(db['address'])
 
     # enable postgis
-    db['con'].cursor().execute("CREATE EXTENSION postgis;")
+    if not exists:
+        db['con'].cursor().execute("CREATE EXTENSION postgis;")
+        db['con'].commit()
 
     logger.info('Database connection established')
     return(db)
 
 
-############## Create Origin/Block Table in SQL ##############
 def init_origins(db, config):
-    '''
-    create a table with the origin blocks
-    '''
-    projected_origin_file = config['set_up']['origin_file_directory'][:-4] + '_projected.shp'
+    # variables
+    con = db['con']
+    engine = db['engine']
     projection = config['set_up']['projection']
+    # import and project
     origin = gpd.read_file(r'{}'.format(config['set_up']['origin_file_directory']))
     origin = origin.to_crs("EPSG:{}".format(projection))
-    origin.to_file(r'{}'.format(projected_origin_file))
-    if config['set_up']['origin_file_directory'] is not False:
-        # db connections
-        db['passw'] = open('pass.txt', 'r').read().strip('\n')
-        export_origin = "shp2pgsql -I -s {} {} origin | PGPASSWORD='{}' psql -U postgres -d access_{} -h 132.181.102.2 -p 5001".format(config['set_up']['projection'], projected_origin_file, db['passw'], config['location']['state'])
-        print(export_origin)
-        subprocess.call(export_origin.split(), stdin=subprocess.PIPE, stdout=open(os.devnull, 'wb'))
-        # subprocess.run(export_origin.split(), shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
-        logger.info('Successfully exported origin shapefile to SQL')
+    # remove MultiPolygons
+    origin = _explode(origin)
+    # export to sql
+    origin['geom'] = origin['geometry'].apply(lambda x: WKTElement(x.wkt, srid=projection))
+    origin = origin[['geoid10','geom']]
+    origin.set_index('geoid10')
+    origin.to_sql('origin', engine, if_exists='replace', dtype={'geom': Geometry('POLYGON', srid= projection)})
+    logger.info('Successfully exported origin shapefile to SQL')
 
 
-############## Create Destination Table in SQL ##############
+def _explode(indf):
+    # https://stackoverflow.com/a/64897035/5890574
+    count_mp = 0
+    outdf = gpd.GeoDataFrame(columns=indf.columns)
+    outdf = indf[indf.geometry.type == 'Polygon']
+    indf = indf[indf.geometry.type != 'Polygon']
+    for idx, row in indf.iterrows():
+        if type(row.geometry) == MultiPolygon:
+            count_mp = count_mp + 1
+            multdf = gpd.GeoDataFrame(columns=indf.columns)
+            recs = len(row.geometry)
+            multdf = multdf.append([row]*recs,ignore_index=True)
+            for geom in range(recs):
+                multdf.loc[geom,'geometry'] = row.geometry[geom]
+            outdf = outdf.append(multdf,ignore_index=True)
+        else:
+            print(row)
+    print("There were ", count_mp, "Multipolygons found and exploded")
+    return outdf
+
+
 def init_destinations(db, config):
     '''
     create the table of destinations
     '''
-    if config['set_up']['destination_file_directory'] != False:
+    if config['set_up']['destination_file_directory'] is not False:
         # db connections
         con = db['con']
         engine = db['engine']
