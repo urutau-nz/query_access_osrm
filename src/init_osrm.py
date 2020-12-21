475908b6847e6136f40e0edd461a20f9ad85958c
Tom Logan <tom.logan@canterbury.ac.nz>
2020-12-21 17:21:33 +1300

    making pbf link location general

--- src/init_osrm.py
+++ src/init_osrm.py
@@ -11,9 +11,8 @@ def main(config, logger):
     mode_dict = {'driving':'car','walking':'foot','cycling':'bicycle'}
 
     # pull the variables from the config file
-    state_name = config['OSM']['state']
-    continent = config['OSM']['continent']
-    country = config['OSM']['country']
+    osm_subregion = config['OSM']['osm_subregion']
+    osm_region = config['OSM']['osm_region']
     port = config['OSRM']['port']
     transport_mode = mode_dict[config['transport_mode']]
     directory = config['OSM']['data_directory']
@@ -28,7 +27,7 @@ def main(config, logger):
         subprocess.run(com.split())
 
     # download the data
-    download_data = 'wget -N https://download.geofabrik.de/{}/{}-latest.osm.pbf -P {}'.format(continent, country, directory)
+    download_data = 'wget -N https://download.geofabrik.de/{}/{}-latest.osm.pbf -P {}'.format(osm_region, osm_subregion, directory)
     p = subprocess.run(download_data.split(), stderr=subprocess.PIPE, bufsize=0)
 
     # if the data does not redownload, it does not need to re-compile.
@@ -36,14 +35,14 @@ def main(config, logger):
         logger.info('Compiling the data files')
         shell_commands = [
                         # init docker data
-                        'docker run -t -v {}:/data osrm/osrm-backend osrm-extract -p /opt/{}.lua /data/{}-latest.osm.pbf'.format(directory, transport_mode, state_name),
-                        'docker run -t -v {}:/data osrm/osrm-backend osrm-partition /data/{}-latest.osrm'.format(directory, state_name),
-                        'docker run -t -v {}:/data osrm/osrm-backend osrm-customize /data/{}-latest.osrm'.format(directory, state_name),
+                        'docker run -t -v {}:/data osrm/osrm-backend osrm-extract -p /opt/{}.lua /data/{}-latest.osm.pbf'.format(directory, transport_mode, osm_subregion),
+                        'docker run -t -v {}:/data osrm/osrm-backend osrm-partition /data/{}-latest.osrm'.format(directory, osm_subregion),
+                        'docker run -t -v {}:/data osrm/osrm-backend osrm-customize /data/{}-latest.osrm'.format(directory, osm_subregion),
                         ]
         for com in shell_commands:
             subprocess.run(com.split(), stdout=open(os.devnull, 'wb'))
     else:
         logger.info('Data not re-downloaded and compiled because no changes to online version')
 
-    run_docker = 'docker run -d --name osrm-{} -t -i -p {}:5000 -v {}:/data osrm/osrm-backend osrm-routed --algorithm mld --max-table-size 100000 /data/{}-latest.osrm'.format(state, port, directory, state_name)
+    run_docker = 'docker run -d --name osrm-{} -t -i -p {}:5000 -v {}:/data osrm/osrm-backend osrm-routed --algorithm mld --max-table-size 100000 /data/{}-latest.osrm'.format(state, port, directory, osm_subregion)
     subprocess.run(run_docker.split())
